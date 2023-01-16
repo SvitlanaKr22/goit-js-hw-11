201a5c4a55fd6b14998ab5d77c9cc12a1c1912a5
Svitlana Kravets <svetakravez72@gmail.com>
2023-01-16 19:55:54 +0200

    final version

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,8 @@
 //Створи фронтенд частину застосунку пошуку і перегляду зображень за ключовим словом.
 // Додай оформлення елементів інтерфейсу.
-// user_id:32806243
-//32806243-dfe26fa8cb817022a28999782
-
 // У відповіді буде масив зображень, що задовольнили критерії параметрів запиту.
-//Кожне зображення описується об'єктом, з якого тобі цікаві тільки наступні властивості:
 
+//Кожне зображення описується об'єктом, з якого тобі цікаві тільки наступні властивості:
 // webformatURL - посилання на маленьке зображення для списку карток.
 // largeImageURL - посилання на велике зображення.
 // tags - рядок з описом зображення. Підійде для атрибуту alt.
@@ -26,7 +23,6 @@ Notify.init({
   width: '400px',
   fontSize: '20px',
   position: 'right-top',
-  //background: '#32c682',
   info: {
     background: '#e978d0',
   },
@@ -34,6 +30,7 @@ Notify.init({
 
 const THROTLLE_DELAY = 300;
 const NUM_IMG = 40;
+
 const parQuery = {
   nameQuery: '',
   page: null,
@@ -47,56 +44,38 @@ const galleryBox = new SimpleLightbox('.gallery a', {
   animationSpeed: 500,
   fadeSpeed: 200,
 });
+
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('#search-form');
+
 let isEvtSubit = false;
+
 form.addEventListener('submit', onFormSubmit);
 document.addEventListener('scroll', throttle(onWindowScroll, THROTLLE_DELAY));
 
-function onFormSubmit(evt) {
-  isEvtSubit = true;
+async function onFormSubmit(evt) {
   evt.preventDefault();
   gallery.innerHTML = '';
+  isEvtSubit = true;
   const { searchQuery } = evt.target.elements;
   parQuery.nameQuery = searchQuery.value;
   parQuery.page = 1;
-  console.log('вызов openGallery в onFormSubmit', parQuery.page);
-  openGallery();
+
+  await openGallery();
 }
 
-function onWindowScroll() {
+async function onWindowScroll() {
   if (isEvtSubit) {
     isEvtSubit = false;
     return;
   }
-
-  // получаем высоту элемента, на котором произошло событие
-  // Нам потребуется знать высоту документа и высоту экрана:
-  const height = document.body.offsetHeight;
-  const screenHeight = window.innerHeight;
-  // Записываем, сколько пикселей пользователь уже проскроллил:
-  const scrolled = window.scrollY;
-
-  // Обозначим порог, по приближении к которому
-  // будем вызывать какое-то действие.
-  const threshold = height - screenHeight / 4;
-  // console.log('порог:', threshold);
-  // Отслеживаем, где находится низ экрана относительно страницы:
-  const position = scrolled + screenHeight;
-  //  console.log('текущая позиция:', position);
-  if (position >= threshold) {
-    // Если мы пересекли полосу-порог, вызываем нужное действие.
-    console.log('We are on the place!!!');
-    parQuery.page += 1;
-    console.log('page:', parQuery.page);
-    openGallery();
-  }
+  await infiniteScroll();
 }
 
 async function openGallery() {
   try {
     const {
-      data: { hits },
+      data: { hits, totalHits },
     } = await fetchContents(parQuery);
 
     if (!hits.length) {
@@ -105,13 +84,17 @@ async function openGallery() {
       );
       return;
     }
+
     gallery.innerHTML += createMarkUp(hits);
     galleryBox.refresh();
-    console.log('номер страницы в openGallery', parQuery.page);
-    if (parQuery.page > 1) smoothScroll();
+
+    if (parQuery.page > 1) {
+      smoothScroll();
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
   } catch (error) {
     console.log(error);
-    console.log('это ошибка');
+    Notify.failure(`Oops!  ${error}`);
   }
 }
 
@@ -122,4 +105,16 @@ function smoothScroll() {
     top: cardHeight * 2,
     behavior: 'smooth',
   });
+}
+
+async function infiniteScroll() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const threshold = height - screenHeight / 4;
+  const position = scrolled + screenHeight;
+  if (position >= threshold) {
+    parQuery.page += 1;
+    await openGallery();
+  }
 }
